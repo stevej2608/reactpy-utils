bf25692de9ef12693bfe41b49022238af8683b6c
Steve Jones <jonesst2608@gmail.com>
2024-11-19 18:16:04 +0000

    Tidy up

--- src/reactpy_utils/types.py
+++ src/reactpy_utils/types.py
@@ -1,16 +1,21 @@
 from typing import Any, Callable
 
 EventArgs = dict[str, Any]
+"""Event handler args type"""
 
 EventHandler = Callable[[EventArgs], None]
+"""Event handler type"""
 
 Action = Callable[..., None]
+"""A callable type that returns None"""
 
 Props = dict[str, Any]
+"""A props dict type"""
+
+PropsFunc = Callable[..., dict[str, Any]]
+"""A callable type that returns a props dict"""
 
 
 def NO_PROPS() -> dict[str, Any]:
+    """A function that returns an empty props dict"""
     return {}
-
-
-PropsFunc = Callable[..., dict[str, Any]]
