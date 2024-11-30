045324ddf7ee43ce70adecb6aaecdccc37e33661
Steve Jones <jonesst2608@gmail.com>
2024-11-30 12:03:39 +0000

    Bump version 0.0.24

--- src/reactpy_utils/__init__.py
+++ src/reactpy_utils/__init__.py
@@ -1,7 +1,7 @@
 # pyright: reportUnusedImport=false
 # ruff: noqa: F401
 
-__version__ = "0.0.23"
+__version__ = "0.0.24"
 
 from reactpy_utils.class_component import class_component
 from reactpy_utils.copy_to_clipboard import CopyToClipboard
@@ -15,7 +15,7 @@ from reactpy_utils.dynamic_context import (
 from reactpy_utils.local_storage import LocalStorageAgent, LocalStorageProvider
 from reactpy_utils.props import props
 from reactpy_utils.script import Script
+from reactpy_utils.static_counter import ID
 from reactpy_utils.unique_sequence import UID
 from reactpy_utils.use_query import use_params, use_search_params
 from reactpy_utils.when import When
-from reactpy_utils.static_counter import ID
