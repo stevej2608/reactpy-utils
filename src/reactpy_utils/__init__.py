b37a6f6e0fe040bdf5c2da28ce027856c05fa79f
Steve Jones <jonesst2608@gmail.com>
2024-12-12 10:10:15 +0000

    Bump version (0.0.29)

--- src/reactpy_utils/__init__.py
+++ src/reactpy_utils/__init__.py
@@ -1,7 +1,7 @@
 # pyright: reportUnusedImport=false
 # ruff: noqa: F401
 
-__version__ = "0.0.28"
+__version__ = "0.0.29"
 
 from reactpy_utils.class_component import class_component
 from reactpy_utils.copy_to_clipboard import CopyToClipboard
