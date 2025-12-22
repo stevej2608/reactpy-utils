f6a5af1d33289795a1ea35c76feb6206da0e8b6d
Steve Jones <jonesst2608@gmail.com>
2025-12-22 11:08:04 +0000

    Tidy up

--- tests/tooling/wait_stable.py
+++ tests/tooling/wait_stable.py
@@ -1,13 +1,7 @@
-import asyncio
-import os
-
-from playwright.async_api._generated import Page
-
-GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS", "").lower() == "true"
-CLICK_DELAY = 250 if GITHUB_ACTIONS else 25  # Delay in milliseconds.
+from playwright.async_api import Page
 
 
-async def page_stable(page: Page) -> None:
+async def wait_page_stable(page: Page) -> None:
     await page.wait_for_load_state("networkidle")
     await page.wait_for_load_state("domcontentloaded")
-    await asyncio.sleep(CLICK_DELAY / 1000)
+    await page.wait_for_timeout(2000)
